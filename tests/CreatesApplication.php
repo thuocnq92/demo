6a01f5a78e6ecad595099251ecbd848c4f3f9fa8
thuocnq92 <thuocnq@rikkeisoft.com>
2021-05-12 16:33:19 +0700

    Update CreatesApplication.php

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -16,6 +16,8 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+        
+        echo 'huhu';
 
         return $app;
     }
